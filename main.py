a8c8a3eee3d5544416976d91982fde4db5914726
KimPolar <128953651+KimPolar@users.noreply.github.com>
2023-12-13 01:05:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/leaderboards/")
+async def leader(season:str, server:str):
+    with open('leaderboards.json') as f:
+        a = json.load(f)
+    return a[season][server]
