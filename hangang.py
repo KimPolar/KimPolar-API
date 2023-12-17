616741cda35a2c5eddb0b60e3e2ddc93b81c3cec
KimPolar <128953651+KimPolar@users.noreply.github.com>
2023-12-18 01:08:30 +0900

    Update hangang.py

--- hangang.py
+++ hangang.py
@@ -6,6 +6,7 @@ def hangang():
   req = requests.get("http://openapi.seoul.go.kr:8088/sample/json/WPOSInformationTime/1/5/").json()
   for x in req['WPOSInformationTime']['row']:
     msr_time = datetime.strptime(x['MSR_DATE']+" "+x['MSR_TIME'].zfill(5)+":00", "%Y%m%d %H:%M:%S")
+    msr_time = msr_time.replace("24:00:00","00:00:00")
     msr_time = msr_time.strftime('%Y-%m-%d %H:%M:%S')
     res.append({'station':x['SITE_ID'], "Temp":x['W_TEMP'], "pH":x['W_PH'], "LastUpdated":msr_time})
   return res
