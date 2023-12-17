1dd2c877009565e252421d1f7c813a48befa677e
KimPolar <128953651+KimPolar@users.noreply.github.com>
2023-12-17 21:35:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import krmetro as km
 import json
 
 app = FastAPI()
@@ -12,3 +13,7 @@ async def leader(season:str, server:str):
     with open('leaderboards.json') as f:
         a = json.load(f)
     return a[season][server]
+
+@app.get("/metro/korea/")
+async def krmetro(lineNum:str):
+    return km(lineNum)
