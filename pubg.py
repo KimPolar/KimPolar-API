5f1e1f9a4cd7d2cc419a4cd480cc2454b86e2fc8
KimPolar <128953651+KimPolar@users.noreply.github.com>
2023-12-27 00:10:10 +0900

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -7,16 +7,25 @@ def finder(accountId):
   }
   
   if accountId.find("account.") != 0:
-    accountId = requests.get(f"https://api.pubg.com/shards/steam/players?filter[playerNames]={accountId}", headers=headers).json()['data'][0]['id']
+    accountId = requests.get(f"https://api.pubg.com/shards/steam/players?filter[playerNames]={accountId}", headers=headers)
+    if accountId.status_code != 200:
+      return {"code":accountId.status_code}
+    accountId = accountId.json()['data'][0]['id']
   weap = requests.get(f"https://prod-live-front.playbattlegrounds.com/").text.split('href="')[1].split("index-steam")[0]
   weap = requests.get(f"https://prod-live-front.playbattlegrounds.com{weap}app/_lib/Bro/translation/data/data_ko.json").json()
   
-  survivalmastery = requests.get(f"https://api.pubg.com/shards/steam/players/{accountId}/survival_mastery", headers=headers).json()
-  
-  weapmastery = requests.get(f"https://api.pubg.com/shards/steam/players/{accountId}/weapon_mastery", headers=headers).json()
+  survivalmastery = requests.get(f"https://api.pubg.com/shards/steam/players/{accountId}/survival_mastery", headers=headers)
+  if survivalmastery.status_code != 200:
+    return {"code":survivalmastery.status_code}
+  survivalmastery = survivalmastery.json()
+
+  weapmastery = requests.get(f"https://api.pubg.com/shards/steam/players/{accountId}/weapon_mastery", headers=headers)
+  if weapmastery.status_code != 200:
+    return {"code":weapmastery.status_code}
+  weapmastery = weapmastery.json()
   weaps = list()
   for x in weapmastery['data']['attributes']['weaponSummaries'].keys():
     weaps.append({"WeapNm":weap["INGAME_ITEM:"+x]['name'],"Tier":weapmastery['data']['attributes']['weaponSummaries'][x]['TierCurrent'],"Level":weapmastery['data']['attributes']['weaponSummaries'][x]['LevelCurrent']})
-  res = {"accountId":accountId, "Survival":{"Tier":survivalmastery['data']['attributes']['tier'],"Level":survivalmastery['data']['attributes']['level']}, "Weapon":weaps}
-  
+  res = {"code":200,"data":{"accountId":accountId, "Survival":{"Tier":survivalmastery['data']['attributes']['tier'],"Level":survivalmastery['data']['attributes']['level']}, "Weapon":weaps}}
+ 
   return res
